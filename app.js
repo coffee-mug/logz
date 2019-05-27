e55b4eb0997acd8fbcab5893a9a4779dfc2d5bcd
cofee-mug <lucas.kostka@escdijon.eu>
2019-05-27 22:52:04 +0200

    use venv variables

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const Koa = require('koa');
 const { router } = require('./features');
 const { MongoClient } = require('mongodb')
 
-const connectionString = 'mongodb+srv://betepoilue:georgesleyeti54@cluster0-meyhp.mongodb.net/test?retryWrites=true'
+const username = process.env.username
+const password = process.env.db_password
+
+const connectionString = `mongodb+srv://${username}:${password}@cluster0-meyhp.mongodb.net/test?retryWrites=true`
 const client = new MongoClient(connectionString, { useNewUrlParser: true });
 
 const app = new Koa();
